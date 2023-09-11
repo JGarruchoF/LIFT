ad0e42aa9451e1f500112e1242462ae3b5596b5f
Javier <jgarrucho1998@gmail.com>
2023-09-11 20:34:47 +0200

    feat(exercises): add admin views

--- LIFT/exercises/admin.py
+++ LIFT/exercises/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from exercises.models import Exercise
+from exercises.models import Exercise, Muscle, Equipment
 
+@admin.register(Muscle)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Equipment)
+class ExerciseAdmin(admin.ModelAdmin):
+    pass
 @admin.register(Exercise)
 class ExerciseAdmin(admin.ModelAdmin):
     pass
