03c60a93d4a46609dba27bc86ea2201e6354d9d8
Javier <jgarrucho1998@gmail.com>
2023-09-11 20:40:52 +0200

    feat(training): add str methods and make fields nullable

--- LIFT/training/models.py
+++ LIFT/training/models.py
@@ -11,17 +11,24 @@ class TrainingSession(TimeStampedModel):
 
 
 class ExerciseSession(TimeStampedModel):
-    session = models.ForeignKey(TrainingSession, on_delete=models.CASCADE, null=False)
+    training_session = models.ForeignKey(TrainingSession, on_delete=models.CASCADE, related_name="exercise_sessions", null=False)
     exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, null=False)
-    
+    def __str__(self):
+        return f"{self.id}"
 
 class Set(models.Model):
     exercise_session = models.ForeignKey(ExerciseSession, on_delete=models.CASCADE, related_name='sets')
-    weight = models.FloatField()
-    reps = models.IntegerField()
+    weight = models.FloatField(blank=True, null=True)
+    reps = models.IntegerField(blank=True, null=True)
     done = models.BooleanField(default=False)
     failure = models.BooleanField(default=False)
     warm_up = models.BooleanField(default=False)
+
+    def __str__(self):
+        if self.done:
+            return f"{self.exercise_session.exercise.name} ({self.weight} kg): {self.reps} reps"
+        else: 
+            return f"{self.exercise_session.exercise.name}: undone"
 
 
 
